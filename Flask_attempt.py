547340d8892c9092a664d095ded36dd705ad97cb
JacekKarnowka <jacek.karnowka@gmail.com>
2022-03-13 11:22:48 +0100

    Created Flask_attempt python file, to try uploading zip files without saving on local drive.

--- Flask_attempt.py
+++ Flask_attempt.py
@@ -3,7 +3,7 @@
 #
 import base64
 import uuid
-import os
+import os, shutil
 import os.path
 import dash_uploader as du
 import dash
@@ -19,6 +19,11 @@ import plotly.io as pio
 import os
 from zipfile import ZipFile
 import xlsxwriter
+import glob
+import base64
+import os
+from urllib.parse import quote as urlquote
+from flask import Flask, send_from_directory
 
 global main_project_name
 global project_number
@@ -29,6 +34,26 @@ global def_option
 plotly_template = pio.templates["plotly_dark"]
 
 path_all = None
+
+UPLOAD_DIRECTORY = "/project_tmp/app_uploaded_files"
+
+
+def clear_directory(path):
+
+    folder = ('{}'.format(urlquote(path)))
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+
+if not os.path.exists(UPLOAD_DIRECTORY):
+    os.makedirs(UPLOAD_DIRECTORY)
 
 def_variable = "Vsc Scenario2"
 def_sort_by = "Window Ref"
@@ -155,7 +180,6 @@ def get_all_options():
 
 
 def bar_chart_figure():
-
     if path_all == '':
         return 0
 
@@ -211,8 +235,8 @@ def bar_chart_figure():
     fig.update_yaxes(title_text="Number",
                      gridcolor=GRAPH_LINES_COLOR,
                      linecolor=GRAPH_LINES_COLOR,
-                     gridwidth= bar_chart_grid_width,
-                     linewidth= bar_chart_line_width,
+                     gridwidth=bar_chart_grid_width,
+                     linewidth=bar_chart_line_width,
                      zerolinecolor=GRAPH_LINES_COLOR),
 
     fig.update_xaxes(title_text='Option',
@@ -315,14 +339,17 @@ def get_upload_component(id):
 
 
 def open_zip(file_name):
-    with ZipFile(file_name, 'r') as zip:
-        zip.printdir()
-        zip.extractall()
+    if 'Data' not in os.listdir('{}'.format(UPLOAD_DIRECTORY)):
+        os.mkdir('{}/Data'.format(UPLOAD_DIRECTORY))
 
+    with ZipFile(file_name, 'r') as zip:
+        # zip.printdir()
+        zip.extractall('{}/Data'.format(UPLOAD_DIRECTORY))
+    # get_a_list('{}/Data/Data'.format(UPLOAD_DIRECTORY))
 
 def update_layout():
     return html.Div([
-        dcc.Location(id='url', refresh=True),
+        dcc.Location(id='url', refresh=False),
         html.Div(id='page-content')
     ])
 
@@ -339,8 +366,8 @@ def default_graph():
                                               'color': 'black'},
                                      }, ))
     fig['layout'].update(
-        title_text= "Upload data and choose proper option",
-        title_x = 0.5,
+        title_text="Upload data and choose proper option",
+        title_x=0.5,
         showlegend=False,
         height=450,
         plot_bgcolor=PLOT_BGCOLOR,
@@ -367,9 +394,21 @@ def default_graph():
                                  'color': '#efb636'})
     return fig
 
+clear_directory(UPLOAD_DIRECTORY)
+print(os.listdir('{}'.format(UPLOAD_DIRECTORY)))
 
-app = dash.Dash('VSC-analysis', external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
+server = Flask(__name__)
+
+app = dash.Dash('VSC-analysis', external_stylesheets=[dbc.themes.BOOTSTRAP], server=server)
+
+# server = app.server
+
+@server.route("/download1/<path:path>")
+def download(path):
+    """Serve a file from the upload directory."""
+    return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
+
+
 UPLOAD_FOLDER_ROOT = r".\Project"
 
 du.configure_upload(app, UPLOAD_FOLDER_ROOT)
@@ -402,7 +441,24 @@ app.layout = html.Div(children=[
         html.Div([
             html.Div(
                 [
-                    get_upload_component(id='dash-uploader'),
+                    # get_upload_component(id='dash-uploader'),
+                    dcc.Upload(
+                                id="upload-data",
+                                children=html.Div(
+                                    ["Drag and drop or select a file to upload."]
+                                ),
+                                style={
+                                    "width": "90%",
+                                    "height": "60px",
+                                    "lineHeight": "60px",
+                                    "borderWidth": "1px",
+                                    "borderStyle": "dashed",
+                                    "borderRadius": "5px",
+                                    "textAlign": "center",
+                                    "margin": "10px",
+                                },
+                                multiple=True,
+                            ),
                     html.Div(id='callback-output'),
                     dcc.Link(html.Button(id='Refresh',
                                          n_clicks=0,
@@ -421,6 +477,8 @@ app.layout = html.Div(children=[
                     'padding': '10px',
                     'display': 'inline-block'
                 }),
+            html.H4("File List", style= TEXT_STYLE),
+            html.Ul(id="file-list"),
             html.H4('Choose option:', style=TEXT_STYLE),
 
             dcc.Checklist(
@@ -472,7 +530,7 @@ app.layout = html.Div(children=[
               'marginLeft': '0px',
               'marginTop': '0px',
               'border': '0px solid black',
-              'height': '900px',
+              'height': '1200px',
               'color': '#657895',
               'white-space': 'nowrap',
               'text-overflow': 'ellipsis',
@@ -594,8 +652,8 @@ app.layout = html.Div(children=[
               'marginLeft': '0px',
               'marginTop': '0px',
               'border': '0px solid black',
-              'height': '900px',
-              'maxHeight': '900px',
+              'height': '1200px',
+              'maxHeight': '1200px',
               'overflow': 'auto',
               'background-color': PLOT_BGCOLOR,
               'position': 'absolute'
@@ -609,6 +667,9 @@ app.layout = html.Div(children=[
               # [Input('option_picker', 'value'),
               [Input('Refresh', 'n_clicks')])
 def option_refresh(value):
+    open_files()
+    # open_zip('{}/Data.zip'.format(UPLOAD_DIRECTORY))
+
     return get_all_options()
 
 
@@ -619,30 +680,31 @@ def option_refresh(value):
     return bar_chart_figure()
 
 
-@du.callback(
-    output=Output('callback-output', 'children'),
-    id='dash-uploader',
-)
-def get_a_list(filenames):
-    open_zip(r'.\Project\uploaded_folder\Data.zip')
-
-    global img_base_path
-    global path_all
-    global main_project_name
-    global project_number
-    global all_options
-    global def_option
-
-    main_project_name = os.listdir(r'.\Data')
-
-    project_number = str(main_project_name[0]).split('-')[1]
-
-    img_base_path = r'.\Data\{}\Results'.format(main_project_name[0])
-    path_all = r'.\Data\{}\{}-3d-VSC-APSH-template_Waldram\Reports'.format(main_project_name[0], project_number)
-    all_options = get_all_options()
-    def_option = all_options[0]
-    # bar_chart_figure()
-    return "Uploaded successfully!"
+# @app.callback(
+#     Output('callback-output', 'children'),
+#     [Input("upload-data", "filename"), Input("upload-data", "contents")]
+# )
+#
+# def get_a_list(filenames):
+#     # open_zip('{}\Data.zip'.format(urlquote(UPLOAD_DIRECTORY)))
+#
+#     global img_base_path
+#     global path_all
+#     global main_project_name
+#     global project_number
+#     global all_options
+#     global def_option
+#
+#     main_project_name = os.listdir('{}\Data'.format(urlquote(UPLOAD_DIRECTORY)))
+#
+#     project_number = str(main_project_name[0]).split('-')[1]
+#
+#     img_base_path = r'{}\Data\{}\Results'.format(urlquote(UPLOAD_DIRECTORY), main_project_name[0])
+#     path_all = r'{}\Data\{}\{}-3d-VSC-APSH-template_Waldram\Reports'.format(urlquote(UPLOAD_DIRECTORY), main_project_name[0], project_number)
+#     all_options = get_all_options()
+#     def_option = all_options[0]
+#     # bar_chart_figure()
+#     return "Uploaded successfully!"
 
 
 @app.callback(
@@ -917,5 +979,65 @@ def generate_excel(n_clicks, data, option):
     return 0
 
 
+def save_file(name, content):
+    """Decode and store a file uploaded with Plotly Dash."""
+    data = content.encode("utf8").split(b";base64,")[1]
+    with open(os.path.join(UPLOAD_DIRECTORY, name), "wb") as fp:
+        fp.write(base64.decodebytes(data))
+
+
+def uploaded_files():
+    """List the files in the upload directory."""
+    files = []
+    for filename in os.listdir(UPLOAD_DIRECTORY):
+        path = os.path.join(UPLOAD_DIRECTORY, filename)
+        if os.path.isfile(path):
+            files.append(filename)
+    return files
+
+
+def file_download_link(filename):
+    """Create a Plotly Dash 'A' element that downloads a file from the app."""
+    location = "/download1/{}".format(urlquote(filename))
+    return html.A(filename, href=location)
+
+
+@app.callback(
+    Output("file-list", "children"),
+    [Input("upload-data", "filename"), Input("upload-data", "contents")],
+)
+def update_output(uploaded_filenames, uploaded_file_contents):
+    """Save uploaded files and regenerate the file list."""
+
+    if uploaded_filenames is not None and uploaded_file_contents is not None:
+        for name, data in zip(uploaded_filenames, uploaded_file_contents):
+            save_file(name, data)
+
+    files = uploaded_files()
+
+    if len(files) == 0:
+        return [html.Li("No files yet!")]
+    else:
+        return [html.Li(file_download_link(filename)) for filename in files]
+
+# #
+#
+#
+def open_files():
+    global path_all
+    global img_base_path
+    global def_option
+
+    open_zip('{}/Data.zip'.format(UPLOAD_DIRECTORY))
+    main_project_name = os.listdir('{}/Data/Data'.format(urlquote(UPLOAD_DIRECTORY)))
+
+    project_number = str(main_project_name[0]).split('-')[1]
+
+    img_base_path = r'{}/Data/Data/{}/Results'.format(urlquote(UPLOAD_DIRECTORY), main_project_name[0])
+    path_all = r'{}/Data/Data/{}/{}-3d-VSC-APSH-template_Waldram/Reports'.format(urlquote(UPLOAD_DIRECTORY), main_project_name[0], project_number)
+    all_options = get_all_options()
+    def_option = all_options[0]
+
+
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug= False)
